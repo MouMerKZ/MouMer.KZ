59dbb72d7b73c8de45125c4f1804e3c77d978998
MouMerKZ <bekzatazbaev@gmail.com>
2024-06-07 18:03:47 +0500

    first commit

--- MouMerKZ/MouMerKZ/wsgi.py
+++ MouMerKZ/MouMerKZ/wsgi.py
@@ -9,6 +9,10 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 
 import os
 
+import sys
+
+sys.path.append('/opt/render/project/src')
+
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MouMerKZ.settings')
